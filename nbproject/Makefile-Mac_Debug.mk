8d9f28c53fee5a3b7bd6002f652fa75c14a7a54e
david_fiser <>
2018-02-02 17:13:58 +0100

    build script changes

--- nbproject/Makefile-Mac_Debug.mk
+++ nbproject/Makefile-Mac_Debug.mk
@@ -15,8 +15,8 @@ NM=nm
 CCADMIN=CCadmin
 RANLIB=ranlib
 CC=clang
-CCC=clang++
-CXX=clang++
+CCC=g++
+CXX=g++
 FC=gfortran
 AS=as
 
@@ -47,8 +47,8 @@ OBJECTFILES= \
 CFLAGS=
 
 # CC Compiler Flags
-CCFLAGS=-Waddress-of-temporary
-CXXFLAGS=-Waddress-of-temporary
+CCFLAGS=
+CXXFLAGS=
 
 # Fortran Compiler Flags
 FFLAGS=
@@ -70,37 +70,37 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cppapplication_1: ${OBJECTFILES}
 ${OBJECTDIR}/cpuid_c.o: cpuid_c.cpp
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/cpuid_c.o cpuid_c.cpp
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/cpuid_c.o cpuid_c.cpp
 
 ${OBJECTDIR}/cpuid_c.h.gch: cpuid_c.h
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o "$@" cpuid_c.h
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o "$@" cpuid_c.h
 
 ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
 
 ${OBJECTDIR}/modules/ArchInfo.o: modules/ArchInfo.cpp
 	${MKDIR} -p ${OBJECTDIR}/modules
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/modules/ArchInfo.o modules/ArchInfo.cpp
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/modules/ArchInfo.o modules/ArchInfo.cpp
 
 ${OBJECTDIR}/modules/CpuInfo.o: modules/CpuInfo.cpp
 	${MKDIR} -p ${OBJECTDIR}/modules
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/modules/CpuInfo.o modules/CpuInfo.cpp
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/modules/CpuInfo.o modules/CpuInfo.cpp
 
 ${OBJECTDIR}/modules/PDInfo.o: modules/PDInfo.cpp
 	${MKDIR} -p ${OBJECTDIR}/modules
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/modules/PDInfo.o modules/PDInfo.cpp
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/modules/PDInfo.o modules/PDInfo.cpp
 
 ${OBJECTDIR}/modules/TsxCheck.o: modules/TsxCheck.cpp
 	${MKDIR} -p ${OBJECTDIR}/modules
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/modules/TsxCheck.o modules/TsxCheck.cpp
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/modules/TsxCheck.o modules/TsxCheck.cpp
 
 # Subprojects
 .build-subprojects:
